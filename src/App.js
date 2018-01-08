ded2462415f6be3b28460aa66787118d77c7f2b4
lemosjp01 <33187777+lemosjp01@users.noreply.github.com>
2018-01-07 23:52:34 -0200

    quarto commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import NavBar from './ui/NavBar'
-
+import NavBar from './ui/NavBar';
+import Home from './container/Home';
 class App extends Component {
   render() {
     const logo = 'CLCode'
     return (
       <div className="container">
-       <NavBar logo={logo}/>
+        <NavBar logo={logo}/>
+        <Home />
       </div>
     );
   }
